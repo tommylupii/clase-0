ed85dded40f9f8e7d18e2a39bf4310f530ab0700
Tomás Lupi <tommylupi2004@gmail.com>
2023-11-06 22:10:24 -0300

    cambios

--- actividadEnClase.js
+++ actividadEnClase.js
@@ -1,40 +1,56 @@
-let nombre = "tomas"
-let apellido = "lupi"
-let precio = "0.99"
-let series = [
-  "vikings",
-  "breaking bad",
-  "peaky blinders",
-  "brooklyn nine-nine",
-  "flash",
-  "naruto",
-]
-let pelicuals = [
-  "interestelar",
-  "el padrino",
-  "la isla siniestra",
-  "juego de gemelas",
-  "toy story",
-  "cars",
-]
+// let nombre = "tomas"
+// let apellido = "lupi"
+// let precio = "0.99"
+// let series = [
+//   "vikings",
+//   "breaking bad",
+//   "peaky blinders",
+//   "brooklyn nine-nine",
+//   "flash",
+//   "naruto",
+// ]
+// let pelicuals = [
+//   "interestelar",
+//   "el padrino",
+//   "la isla siniestra",
+//   "juego de gemelas",
+//   "toy story",
+//   "cars",
+// ]
 
-let diasDeLaSemana = [
-  "lunes",
-  "martes",
-  "miercoles",
-  "jueves",
-  "viernes",
-]
+// let diasDeLaSemana = [
+//   "lunes",
+//   "martes",
+//   "miercoles",
+//   "jueves",
+//   "viernes",
+// ]
 
-function valorRandom(arreglo){
-  return arreglo[Math.floor(Math.random() * arreglo.length)]
+// function valorRandom(arreglo){
+//   return arreglo[Math.floor(Math.random() * arreglo.length)]
+// }
+
+// console.log( nombre + " " + apellido)
+// // console.log(series[Math.floor(Math.random() * series.length)])
+// // console.log(pelicuals[Math.floor(Math.random() * pelicuals.length)])
+// // console.log(diasDeLaSemana[Math.floor(Math.random() * diasDeLaSemana.length)])
+
+// console.log(valorRandom(diasDeLaSemana))
+// console.log(valorRandom(series))
+// console.log(valorRandom(pelicuals))
+
+let persona = {
+  nombre: "toimas",
+  apellido: "lupi",
+  nacionalidad: "argneinto",
+  edad: 18,
+  genero: "masculino",
 }
 
-console.log( nombre + " " + apellido)
-// console.log(series[Math.floor(Math.random() * series.length)])
-// console.log(pelicuals[Math.floor(Math.random() * pelicuals.length)])
-// console.log(diasDeLaSemana[Math.floor(Math.random() * diasDeLaSemana.length)])
+console.log(persona);
+persona.edad++
+console.log(persona);
+persona.profesion = "programador"
+persona.pasatiempo = "jugar"
 
-console.log(valorRandom(diasDeLaSemana))
-console.log(valorRandom(series))
-console.log(valorRandom(pelicuals))
+console.log(persona)
